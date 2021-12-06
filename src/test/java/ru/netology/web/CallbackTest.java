1a98f99c308a10df0b96864531db522b653f8ff8
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-12-06 13:00:50 +0300

    add test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,25 +1,29 @@
 package ru.netology.web;
 
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
 
     private WebDriver driver;
 
     @BeforeAll
-    public void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","./driver/mac/chromedriver");
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
 
     }
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -27,18 +31,74 @@ public class CallbackTest {
         driver = new ChromeDriver(options);
     }
 
-    @AfterAll
-    public void tearDown(){
+    @AfterEach
+    public void tearDown() {
         driver.quit();
         driver = null;
 
     }
 
     @Test
-    public void shouldSendForm() {
+    public void shouldSendCorrectForm() {
         driver.get("http://localhost:9999");
-        System.out.println("");
-
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Анна Игнатьева");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79993321232");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
     }
+
+    @Test
+    public void shouldSendCorrectFormWithDoubleName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Анна-Мария Петрова");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79993321232");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
+    }
+
+    @Test
+    public void shouldSendCorrectFormWithDoubleNameWithSpace() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Мария Антуанетта Петрова");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79993321232");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id = 'order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
+    }
+
+    @Test
+    public void shouldSendCorrectFormWithoutNameAndSurname() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("+79993321232");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector(".input_invalid")).getText().trim();
+        String expected = "Фамилия и имя\n" +
+                "Поле обязательно для заполнения";
+        assertEquals(expected, actualText);
+    }
+
+    @Test
+    public void shouldSendCorrectFormWithoutTelNumb() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type = 'text']")).sendKeys("Мария Авдеева");
+        driver.findElement(By.cssSelector("[type = 'tel']")).sendKeys("");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String actualText = driver.findElement(By.cssSelector(".input_invalid")).getText().trim();
+        String expected = "Мобильный телефон\n" +
+                "Поле обязательно для заполнения";
+        assertEquals(expected, actualText);
+    }
+
 
 }
